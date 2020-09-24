64b7567f75a6d98d0373645367e7243b3edaa7fe
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-24 22:15:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,16 +2,16 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   final_answer = data.each_with_object({}) do |(key, value), final_answer|
-  value.each do |in_key, names|
-    names.each do |name|
+    value.each do |in_key, names|
+      names.each do |name|
   # binding.pry
-      if !final_answer[name]
-        final_answer[name] = {}
-      end  
-      if !final_answer[name][key]
-        !final_answer[name][key] = []
+        if !final_answer[name]
+          final_answer[name] = {}
+        end  
+        if !final_answer[name][key]
+          !final_answer[name][key] = []
         binding.pry
-      end
+        end
       final_answer[name][key].push(in_key)
   end
 final_answer
