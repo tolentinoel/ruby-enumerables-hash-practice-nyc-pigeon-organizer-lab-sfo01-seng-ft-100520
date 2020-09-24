7042f329108455014b807f64bc59c4a9967cba7f
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-24 22:25:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,6 @@ def nyc_pigeon_organizer(data)
   final_list = data.each_with_object({}) do |(key, value), final_answer|
     value.each do |in_key, names|
       names.each do |name|
-  # binding.pry
         if !final_answer[name]
           final_answer[name] = {}
         end  
@@ -15,32 +14,8 @@ def nyc_pigeon_organizer(data)
       final_answer[name][key].push(in_key)
       end
     end
-  # final_answer
+  final_answer
   end
-  binding.pry
 end
 
 
-
-
-
-{"Theo"=>{
-  :color=>[:purple, :grey], 
-  :gender=>[:male], 
-  :lives=>["Subway"]},
- "Peter Jr."=>
-  {:color=>[:purple, :grey], 
-  :gender=>[:male], 
-  :lives=>["Library"]},
- "Lucky"=>{
-   :color=>[:purple], 
-   :gender=>[:male], 
-   :lives=>["Central Park"]},
- "Ms. K"=>{
-   :color=>[:grey, :white],
-   :gender=>[:female], 
-   :lives=>["Central Park"]},
- "Queenie"=>{:color=>[:white, :brown], :gender=>[:female], :lives=>["Subway"]},
- "Andrew"=>{:color=>[:white], :gender=>[:male], :lives=>["City Hall"]},
- "Alex"=>
-  {:color=>[:white, :brown], :gender=>[:male], :lives=>["Central Park"]}}
