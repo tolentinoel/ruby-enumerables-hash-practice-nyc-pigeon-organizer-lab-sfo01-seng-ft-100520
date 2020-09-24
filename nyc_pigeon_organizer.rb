f52e0f69392467ec9e08c10ba1049389f63f788c
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-24 22:34:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final_list = data.each_with_object({}) do |(key, value), final_answer|
+    value.each do |in_key, names|
+      names.each do |name|
+        if !final_answer[name]
+          final_answer[name] = {}
+        end  
+        if !final_answer[name][key]
+          !final_answer[name][key] = []
+        end
+      final_answer[name][key].push(in_key.to_s)
+      end
+    end
+  end
 end
+
+
