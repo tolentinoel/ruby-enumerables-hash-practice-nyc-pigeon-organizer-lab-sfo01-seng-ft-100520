36d7d05f3dee40d1d2f5a62f6309ac33a100f348
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-24 22:02:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,8 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_answer|
   value.each do |in_key, names|
     names.each do |name|
-  end  
   binding.pry
+  end  
   end
   final_answer
 binding.pry
